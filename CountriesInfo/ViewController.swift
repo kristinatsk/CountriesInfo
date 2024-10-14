d8af9d2cfd5e402c67a14e0e4b2e3f920b286327
kristinatsk <kristinatsk@gmail.com>
2024-10-14 07:03:33 +0300

    Initial Commit

---
+++ CountriesInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesInfo
+//
+//  Created by Kristina Kostenko on 14.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
